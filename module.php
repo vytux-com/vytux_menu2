eb604b712affe0b25bbd9fd61c99a4c0597e430d
Vytautas Krivickas <vytautas.krivickas@gmail.com>
2015-02-03 21:56:39 +1100

    Zend_Translate work

--- module.php
+++ module.php
@@ -24,7 +24,7 @@ namespace Webtrees;
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-use Zend_Registry;
+use Zend_Translate;
 
 class vytux_menu2_WT_Module extends Module implements ModuleBlockInterface, ModuleConfigInterface, ModuleMenuInterface {
 
@@ -33,17 +33,17 @@ class vytux_menu2_WT_Module extends Module implements ModuleBlockInterface, Modu
 		// Load any local user translations
 		if (is_dir(WT_MODULES_DIR.$this->getName().'/language')) {
 			if (file_exists(WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.mo')) {
-				Zend_Registry::get('Zend_Translate')->addTranslation(
+				I18N::addTranslation(
 					new Zend_Translate('gettext', WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.mo', WT_LOCALE)
 				);
 			}
 			if (file_exists(WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.php')) {
-				Zend_Registry::get('Zend_Translate')->addTranslation(
+				I18N::addTranslation(
 					new Zend_Translate('array', WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.php', WT_LOCALE)
 				);
 			}
 			if (file_exists(WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.csv')) {
-				Zend_Registry::get('Zend_Translate')->addTranslation(
+				I18N::addTranslation(
 					new Zend_Translate('csv', WT_MODULES_DIR.$this->getName().'/language/'.WT_LOCALE.'.csv', WT_LOCALE)
 				);
 			}
